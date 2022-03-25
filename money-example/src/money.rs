f4cf5b0faa47fab358be938aa559e6e7668a590e
guricerin <chanbo1e9@gmail.com>
2022-03-25 20:49:56 +0900

    p.102

--- money-example/src/money.rs
+++ money-example/src/money.rs
@@ -1,4 +1,6 @@
-#[derive(PartialEq)]
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
 pub struct Money {
     amount: i32,
     currency: &'static str,
@@ -32,27 +34,44 @@ impl Money {
     }
 }
 
+impl Add for Money {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            amount: self.amount + other.amount,
+            currency: self.currency,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::money::Money;
 
     #[test]
-    fn test_multiplication() {
+    fn multiplication() {
         let five = Money::dollar(5);
-        assert!(Money::dollar(10) == five.times(2));
-        assert!(Money::dollar(15) == five.times(3));
+        assert_eq!(Money::dollar(10), five.times(2));
+        assert_eq!(Money::dollar(15), five.times(3));
     }
 
     #[test]
-    fn test_equality() {
-        assert!(Money::dollar(5) == Money::dollar(5));
-        assert!(Money::dollar(5) != Money::dollar(6));
-        assert!(Money::franc(5) != Money::dollar(5));
+    fn equality() {
+        assert_eq!(Money::dollar(5), Money::dollar(5));
+        assert_ne!(Money::dollar(5), Money::dollar(6));
+        assert_ne!(Money::franc(5), Money::dollar(5));
     }
 
     #[test]
-    fn test_currency() {
+    fn currency() {
         assert_eq!("USD", Money::dollar(1).currency());
         assert_eq!("CHF", Money::franc(1).currency());
+    }
+
+    #[test]
+    fn simple_addition() {
+        let sum = Money::dollar(5) + Money::dollar(5);
+        assert_eq!(Money::dollar(10), sum);
     }
 }
