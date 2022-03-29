446b8f7ac240695318cc1e15208562fbadb3b6b4
guricerin <chanbo1e9@gmail.com>
2022-03-29 18:35:43 +0900

    p.175

--- xunit-example/src/xunit.rs
+++ xunit-example/src/xunit.rs
@@ -3,6 +3,7 @@ enum TestKind {
 }
 
 struct TestCase {
+    was_setup: bool,
     was_run: bool,
     kind: TestKind,
 }
@@ -10,12 +11,19 @@ struct TestCase {
 impl TestCase {
     fn new(kind: TestKind) -> Self {
         Self {
+            was_setup: true,
             was_run: false,
             kind: kind,
         }
     }
 
+    fn setup(&mut self) {
+        self.was_setup = true;
+        self.was_run = false;
+    }
+
     fn run(&mut self) {
+        self.setup();
         match self.kind {
             TestKind::TestMethod => self.test_method(),
         }
@@ -26,15 +34,36 @@ impl TestCase {
     }
 }
 
+struct TestCaseTest {
+    test: TestCase,
+}
+
+impl TestCaseTest {
+    fn setup(kind: TestKind) -> Self {
+        Self {
+            test: TestCase::new(kind),
+        }
+    }
+
+    fn test_running(&mut self) {
+        self.test.run();
+        assert!(self.test.was_run);
+    }
+
+    fn test_setup(&mut self) {
+        self.test.run();
+        assert!(self.test.was_setup);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::xunit::*;
 
     #[test]
     fn method() {
-        let mut test = TestCase::new(TestKind::TestMethod);
-        assert!(!(test.was_run));
-        test.run();
-        assert!(test.was_run);
+        let mut test = TestCaseTest::setup(TestKind::TestMethod);
+        test.test_running();
+        test.test_setup();
     }
 }
