c315d5a4821b0b6e94ce5fdbd779e1e3f28bea49
guricerin <chanbo1e9@gmail.com>
2022-03-30 16:58:22 +0900

    p.306:

--- fibonacci/src/lib.rs
+++ fibonacci/src/lib.rs
@@ -1,5 +1,9 @@
 fn fib(n: i64) -> i64 {
-    0
+    if n == 0 {
+        0
+    } else {
+        1
+    }
 }
 
 #[cfg(test)]
@@ -9,5 +13,6 @@ mod tests {
     #[test]
     fn fib_test() {
         assert_eq!(0, fib(0));
+        assert_eq!(1, fib(1));
     }
 }
