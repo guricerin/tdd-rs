4d523f79b08aec7768a20cc6eea893e077423060
guricerin <chanbo1e9@gmail.com>
2022-03-30 14:42:38 +0900

    p.202

--- xunit-example/src/xunit.rs
+++ xunit-example/src/xunit.rs
@@ -73,8 +73,7 @@ impl TestCase {
         self.log = "setup ".to_owned();
     }
 
-    fn run(&mut self) -> TestResult {
-        let mut result = TestResult::new();
+    fn run(&mut self, result: &mut TestResult) {
         result.test_started();
         self.setup();
         match self.method.run() {
@@ -82,20 +81,10 @@ impl TestCase {
             Err(_) => result.test_failed(),
         };
         self.teardown();
-        result
     }
 
     fn teardown(&mut self) {
         self.log = format!("{}teardown ", self.log);
-    }
-
-    fn test_method(&mut self) -> Result<()> {
-        self.log = format!("{}test_method ", self.log);
-        Ok(())
-    }
-
-    fn test_broken_method(&mut self) -> Result<()> {
-        Err(anyhow!(""))
     }
 }
 
@@ -112,50 +101,50 @@ impl TestSuite {
         self.tests.push(test);
     }
 
-    fn run(&self) -> TestResult {
-        let mut result = TestResult::new();
-        for test in self.tests.iter() {}
-        todo!()
+    fn run(&mut self, result: &mut TestResult) {
+        for i in 0..self.tests.len() {
+            self.tests[i].run(result);
+        }
     }
 }
 
-struct TestCaseTest {}
+mod test_case_test {
+    use super::*;
 
-impl TestCaseTest {
-    fn new() -> Self {
-        Self {}
-    }
-
-    fn test_template_method(&self) {
+    pub fn test_template_method() {
         let mut test = TestCase::new(TestMethod);
-        test.run();
+        let mut result = TestResult::new();
+        test.run(&mut result);
         assert_eq!("setup test_method teardown ", test.log);
     }
 
-    fn test_result(&self) {
+    pub fn test_result() {
         let mut test = TestCase::new(TestMethod);
-        let result = test.run();
+        let mut result = TestResult::new();
+        test.run(&mut result);
         assert_eq!("1 run, 0 failed", result.summary());
     }
 
-    fn test_failed_result(&self) {
+    pub fn test_failed_result() {
         let mut test = TestCase::new(TestBrokenMethod);
-        let result = test.run();
+        let mut result = TestResult::new();
+        test.run(&mut result);
         assert_eq!("1 run, 1 failed", result.summary());
     }
 
-    fn test_failed_result_formatting(&self) {
+    pub fn test_failed_result_formatting() {
         let mut result = TestResult::new();
         result.test_started();
         result.test_failed();
         assert_eq!("1 run, 1 failed", result.summary());
     }
 
-    fn test_suite(&self) {
+    pub fn test_suite() {
         let mut suite = TestSuite::new();
         suite.add(TestCase::new(TestMethod));
         suite.add(TestCase::new(TestBrokenMethod));
-        let result = suite.run();
+        let mut result = TestResult::new();
+        suite.run(&mut result);
         assert_eq!("2 run, 1 failed", result.summary());
     }
 }
@@ -166,11 +155,10 @@ mod tests {
 
     #[test]
     fn method() {
-        let test = TestCaseTest::new();
-        test.test_template_method();
-        test.test_result();
-        test.test_failed_result();
-        test.test_failed_result_formatting();
-        // test.test_suite();
+        test_case_test::test_template_method();
+        test_case_test::test_result();
+        test_case_test::test_failed_result();
+        test_case_test::test_failed_result_formatting();
+        test_case_test::test_suite();
     }
 }
