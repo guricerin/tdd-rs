cbc96ca1e33fb9825a284e2cf9396cbd5ac05a82
guricerin <chanbo1e9@gmail.com>
2022-03-30 17:05:37 +0900

    p.307

--- fibonacci/src/lib.rs
+++ fibonacci/src/lib.rs
@@ -1,8 +1,8 @@
 fn fib(n: i64) -> i64 {
-    if n == 0 {
-        0
-    } else {
-        1
+    match n {
+        0 => 0,
+        n if n <= 2 => 1,
+        n => 2,
     }
 }
 
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn fib_test() {
-        let cases = vec![(0, 0), (1, 1), (2, 1)];
+        let cases = vec![(0, 0), (1, 1), (2, 1), (3, 2)];
         for v in cases.iter() {
             assert_eq!(v.1, fib(v.0));
         }
